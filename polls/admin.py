909f9d4c77b2aa82ff022bf3ed9e603c67f11b60
kcuyh <khj879123@gmail.com>
2019-12-03 19:20:38 +0900

    admin update

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from polls.models import Question, Choice #테이블이 새로생성되면 admin.py 에 알려줘야한다
+
+admin.site.register(Question)
+admin.site.register(Choice)
 
 # Register your models here.
