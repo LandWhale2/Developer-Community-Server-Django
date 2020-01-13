3567c5499cffc7d0d1b1896375177762860cf41d
kcuyh <khj879123@gmail.com>
2020-01-13 16:19:44 +0900

    [update]socket message

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,52 +1,77 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from chat.models import Message
+from chat.models import Message, Room, RoomMessage
 from chat.serializers import MeSerial, MessageSerializer
+import logging
+import re
 import json
+
+
+
+log = logging.getLogger(__name__)
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
+        print(self.room_name)
+        print(self.room_group_name)
+
+        try:
+            room = Room.objects.get(label = self.room_group_name)
+        except Room.DoesNotExist:
+            print('DoesNotExist')
+            room = Room(label = self.room_group_name)
+            room.save()
+
+        print(room)
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-
-
         await self.accept()
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        sender = text_data_json['sender']
+        # message = text_data_json['message']
+        # handle = text_data_json['handle']
+        room = Room.objects.get(label = self.room_group_name)
+
+        if set(text_data_json.keys())!=set(('handle', 'message')):
+            log.debug('예기치못한 포멧  = %s', text_data_json)
+            return
+
+        if text_data_json:
+            log.debug('chat room : %s handle: %s message %s',
+            room.label, text_data_json['handle'],text_data_json['message'])
+            m = room.messages.create(**text_data_json)
+
 
         # Send message to room group
+    
         await self.channel_layer.group_send(
             self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'sender' : sender
-            }
+            {'text':json.dumps(m.as_dict())}
         )
 
-    @database_sync_to_async
-    def getmessage(self):
-        print(Message.objects.all()[0])
-        return Message.objects.all()[0]
+    # @database_sync_to_async
+    # def getmessage(self):
+    #     print(Message.objects.all()[0])
+    #     return Message.objects.all()[0]
 
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-        sender = event['sender']
+    # async def chat_message(self, event):
+    #     message = event['message']
+    #     handle = event['handle']
+    #     timestamp = event['timestamp']
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'sender' : sender
-        }))
+    #     # Send message to WebSocket
+    #     await self.send(text_data=json.dumps({
+    #         'message': message,
+    #         'handle' : handle,
+    #         'timestamp': timestamp,
+    #     }))
--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,8 @@
+from __future__ import unicode_literals
 from django.db import models
 from users.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -13,3 +16,41 @@ class Message(models.Model):
            return self.message
      class Meta:
            ordering = ('-timestamp',)
+
+
+
+# class Room(models.Model):
+#       roomname = models.C
+#       sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+#       message = models.CharField(max_length=1200)
+#       timestamp = models.DateTimeField(auto_now_add=True)
+#       is_read = models.BooleanField(default= False)
+#       def __str__(self):
+#            return self.message
+#      class Meta:
+#            ordering = ('-timestamp',)
+
+
+class Room(models.Model):
+      name = models.TextField()
+      label = models.SlugField(unique= True)
+
+      def __unicode__(self):
+            return self.label
+
+
+class RoomMessage(models.Model):
+      room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
+      handle = models.TextField()
+      message = models.TextField()
+      timestamp = models.DateTimeField(default = timezone.now, db_index= True)
+
+      def __unicode__(self):
+            return '[{timestamp}] {handle}:{message}'.format(**self.as_dict())
+      
+      @property
+      def formatted_timestamp(self):
+            return self.timestamp.strftime('%b %-d %-I:%M %p')
+      
+      def as_dict(self):
+            return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp}
